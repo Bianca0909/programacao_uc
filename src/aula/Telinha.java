392671c3d9e91ecd06e6b03f9ce269cbadc2c5e3
bianca.medeiros <bianca.medeiros@triersistemas.com.br>
2022-09-22 21:46:42 -0300

    exercicios aula

---
+++ src/aula/Telinha.java
@@ -0,0 +1,187 @@
+package aula;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+
+public class Telinha extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Telinha frame = new Telinha();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Telinha() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 320);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+
+		JLabel lblNewLabel = new JLabel("Calculadora");
+
+		textField = new JTextField();
+		textField.setColumns(10);
+
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+
+		JLabel lblNewLabel_1 = new JLabel("");
+
+		JLabel lblNewLabel_2 = new JLabel("NUMERO 2");
+
+		JLabel lblNewLabel_3 = new JLabel("NUMERO 1");
+
+		JButton btnNewButton = new JButton("Somar");
+
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Double numero1 = Double.parseDouble(textField.getText());
+				Double numero2 = Double.parseDouble(textField_1.getText());
+				Double soma = numero1 + numero2;
+				textField_2.setText(Double.toString(soma));
+			}
+		});
+
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+
+		JButton btnNewButton_1 = new JButton("Subtrair");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				Double numero1 = Double.parseDouble(textField.getText());
+				Double numero2 = Double.parseDouble(textField_1.getText());
+				Double subtracao = numero1 - numero2;
+				textField_3.setText(Double.toString(subtracao));
+			}
+
+		});
+
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+
+		JButton btnNewButton_2 = new JButton("Multiplicar");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Double numero1 = Double.parseDouble(textField.getText());
+				Double numero2 = Double.parseDouble(textField_1.getText());
+				Double multiplicacao = numero1 * numero2;
+				textField_4.setText(Double.toString(multiplicacao));
+			}
+		});
+
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+
+		JButton btnNewButton_3 = new JButton("Dividir");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Double numero1 = Double.parseDouble(textField.getText());
+				Double numero2 = Double.parseDouble(textField_1.getText());
+				Double divisao = numero1 / numero2;
+				textField_5.setText(Double.toString(divisao));
+			}
+		});
+
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
+				.createSequentialGroup().addGap(35)
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
+						.createSequentialGroup()
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addComponent(lblNewLabel_1)
+								.addComponent(lblNewLabel_3).addComponent(lblNewLabel_2))
+						.addGap(4)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE)))
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING).addComponent(lblNewLabel)
+								.addGroup(Alignment.LEADING, gl_contentPane.createParallelGroup(Alignment.TRAILING)
+										.addGroup(gl_contentPane.createSequentialGroup().addComponent(btnNewButton)
+												.addGap(26).addComponent(textField_2, GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+										.addGroup(gl_contentPane.createSequentialGroup().addComponent(btnNewButton_1)
+												.addGap(18).addComponent(textField_3, GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+						.addGroup(gl_contentPane.createSequentialGroup()
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+										.addGroup(gl_contentPane.createSequentialGroup().addComponent(btnNewButton_3)
+												.addGap(24))
+										.addGroup(gl_contentPane.createSequentialGroup().addComponent(btnNewButton_2)
+												.addPreferredGap(ComponentPlacement.UNRELATED)))
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(textField_5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+												GroupLayout.PREFERRED_SIZE)
+										.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+												GroupLayout.PREFERRED_SIZE))))
+				.addContainerGap(196, Short.MAX_VALUE)));
+		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
+				.createSequentialGroup().addContainerGap().addComponent(lblNewLabel).addGap(17)
+				.addGroup(gl_contentPane
+						.createParallelGroup(Alignment.BASELINE).addComponent(lblNewLabel_3).addComponent(textField,
+								GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addGap(18)
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addComponent(lblNewLabel_1)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(lblNewLabel_2)
+								.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE)))
+				.addGap(34)
+				.addGroup(gl_contentPane
+						.createParallelGroup(Alignment.BASELINE).addComponent(btnNewButton).addComponent(textField_2,
+								GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(ComponentPlacement.UNRELATED)
+				.addGroup(gl_contentPane
+						.createParallelGroup(Alignment.BASELINE).addComponent(btnNewButton_1).addComponent(textField_3,
+								GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(ComponentPlacement.RELATED)
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnNewButton_2))
+				.addPreferredGap(ComponentPlacement.UNRELATED)
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(btnNewButton_3)
+						.addComponent(textField_5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE))
+				.addGap(17)));
+		contentPane.setLayout(gl_contentPane);
+	}
+
+}
